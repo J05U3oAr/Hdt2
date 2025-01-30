10dcc5894ba8feb42d2a3ac727049a5ad819a922
Denil Parada <paradadeniljose@gmail.com>
2025-01-29 23:09:40 -0600

    documentacion del stack vector

--- StackVector.java
+++ StackVector.java
@@ -7,45 +7,85 @@
  * Denil Parada - 24761
  * Arodi Chavez - 241112
  * Fecha: 30/01/2025
- * Descripción: -----
+ * Descripción: Implementación de un stack (pila) utilizando la clase Vector de Java. 
+ * Esta clase implementa la interfaz Stack y define las operaciones básicas para una pila.
+ * 
+ * @param <E> El tipo de elementos que se almacenarán en el stack.
  */
 
-import java.util.Vector;
+ import java.util.Vector;
 
-public class StackVector<E> implements Stack<E> 
-{
-    protected Vector<E> data;
-
-    public StackVector() {
-        // post: constructs a new, empty stack
-        data = new Vector<E>();
-    }
-
-    public void push(E item) {
-        // post: the value is added to the stack
-        //       will be popped next if no intervening push
-        data.add(item);
-    }
-
-    public E pop() {
-        // pre: stack is not empty
-        // post: most recently pushed item is removed and returned
-        return data.remove(size() - 1);
-    }
-
-    public E peek() {
-        // pre: stack is not empty
-        // post: top value (next to be popped) is returned
-        return data.get(size() - 1);
-    }
-
-    public int size() {
-        // post: returns the number of elements in the stack
-        return data.size();
-    }
-
-    public boolean empty() {
-        // post: returns true if and only if the stack is empty
-        return size() == 0;
-    }
-}
+ public class StackVector<E> implements Stack<E> {
+ 
+     /**
+      * Vector que almacena los elementos del stack.
+      */
+     protected Vector<E> data;
+ 
+     /**
+      * Constructor que inicializa un stack vacío.
+      * 
+      * @post Se construye un stack vacío.
+      */
+     public StackVector() {
+         data = new Vector<E>();
+     }
+ 
+     /**
+      * Añade un elemento al stack.
+      * 
+      * @param item El elemento que se desea añadir al stack.
+      * @post El elemento se añade al final del stack.
+      */
+     @Override
+     public void push(E item) {
+         data.add(item);
+     }
+ 
+     /**
+      * Remueve y devuelve el elemento más recientemente añadido al stack.
+      * 
+      * @return El elemento que estaba en la cima del stack.
+      * @pre El stack no debe estar vacío.
+      * @post El elemento más reciente es removido y retornado.
+      */
+     @Override
+     public E pop() {
+         return data.remove(size() - 1);
+     }
+ 
+     /**
+      * Devuelve el elemento en la cima del stack sin removerlo.
+      * 
+      * @return El elemento que está en la cima del stack.
+      * @pre El stack no debe estar vacío.
+      * @post Se devuelve el valor en la cima del stack sin modificarlo.
+      */
+     @Override
+     public E peek() {
+         return data.get(size() - 1);
+     }
+ 
+     /**
+      * Devuelve el número de elementos en el stack.
+      * 
+      * @return El número de elementos almacenados en el stack.
+      * @post Devuelve la cantidad de elementos en el stack.
+      */
+     @Override
+     public int size() {
+         return data.size();
+     }
+ 
+     /**
+      * Verifica si el stack está vacío.
+      * 
+      * @return {@code true} si el stack está vacío, {@code false} en caso contrario.
+      * @post Devuelve verdadero si el stack no contiene elementos.
+      */
+     @Override
+     public boolean empty() {
+         return size() == 0;
+     }
+ }
+ 
